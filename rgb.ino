433f76051dc55c0cd8668d5303fbc71d4fc65857
terceiro.com.br <andre@terceiro.com.br>
2019-03-06 00:48:42 -0300

    Update rgb.ino

--- rgb.ino
+++ rgb.ino
@@ -1,34 +1,42 @@
-float s;
-const int seletor = A3; 
-const int Bar_G = 8;
-const int Bar_Y1 = 7;
-const int Bar_Y2 = A5;
-const int Bar_R = A4;
-const int intervalo = 1000;
-
-float ler_sensor() {
-  return analogRead(A1);
-}
-
 void setup() {
-  pinMode(Bar_R, OUTPUT);
-  pinMode(Bar_G, OUTPUT);
-  pinMode(Bar_Y1, OUTPUT);
-  pinMode(Bar_Y2, OUTPUT);
-  pinMode(seletor, OUTPUT);
-  digitalWrite(seletor, HIGH);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);      
 }
 
 void loop() {
-  s = ler_sensor();
-  
-  if (s > 10) {
-    digitalWrite(Bar_R, HIGH);
-    digitalWrite(Bar_Y2, LOW);
-    delay(intervalo);
-  } else {
-    digitalWrite(Bar_Y2, HIGH);
-    digitalWrite(Bar_R, LOW);
-    delay(intervalo);
-  }
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW); 
+    delay(1000);
+
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW); 
+    delay(1000);
+
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH); 
+    delay(1000);
+
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW); 
+    delay(1000);
+    
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH); 
+    delay(1000);
+
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH); 
+    delay(1000);
+
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH); 
+    delay(1000);
 }
